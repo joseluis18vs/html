1acc8fa0a113589078b19bfcea8261dc40ba546d
Jose Vanegas <joseluis18vs@gmail.com>
2022-10-21 21:25:58 +0200

    Remove first frame

--- Frames/Publications_frame/Components/header/header.js
+++
@@ -1,6 +0,0 @@
-let headerNav = document.querySelector(".main_header__navBar");
-let menuShowToggle = document.querySelector(".navBar_menuBtn_toggle");
-
-menuShowToggle.addEventListener("click", () => {
-    headerNav.classList.toggle("show");
-})
